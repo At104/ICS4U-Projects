e83eb7eb7f8b1e3c4ba945a7caa6a70676c62ed4
Atul R | SomethingClever <83225748+At104@users.noreply.github.com>
2024-01-22 00:30:13 -0500

    Unit 2 Upload

---
+++ Unit 2/U2A7_Flight/src/u2a7_flight/U2A7_Flight.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package u2a7_flight;
+
+/**
+ *
+ * @author Atul
+ */
+public class U2A7_Flight {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
